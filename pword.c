4ba6b681ce57fd75ffb194e3423cd4bb77147d9f
Adipto Ghosh <adipto.ghosh@gmail.com>
2026-05-28 16:34:30 +0000

    Initial Commit

---
+++ pword.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void encrypt(char *password, int key){
+
+    key = key % 26;
+
+    for (int i = 0; password[i] != '\0'; i++){
+        if (isupper(password[i])){
+            password[i] = ((password[i] - 'A') + key) % 26 + 'A';
+        } else if (islower(password[i])){
+            password[i] = ((password[i] - 'a') + key) % 26 + 'a';
+        }
+    }
+    printf("%s\n", password);
+}
+
+int main(int argc, char *argv[]){
+    if (argc != 3){
+        printf("ERROR: please provide the following after the program name: [password] [key]\n");
+    }
+
+    char *password = NULL;
+    int key = 0;
+
+    for (int i = 1; i < argc; i++){
+        if (i == 1)
+            password = argv[i];
+        for (int j = 0; argv[i][j] != '\0'; j++){
+            if (i == 2){
+            if (argv[i][j] < '0' || argv[i][j] > '9'){
+                printf("ERROR: please provide a number\n");
+                return 1;
+                }
+                key = atoi(argv[i]);
+            }
+            }
+        }
+    if (password != NULL){
+        encrypt(password, key);
+    }
+
+    return 0;
+}
